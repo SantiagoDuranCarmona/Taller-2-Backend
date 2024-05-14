e71d5971b69f2dece83e497cbee8e90430296330
SantiagoDC <sandurcar@gmail.com>
2024-05-14 13:15:41 -0500

    Added business logic to the service and initial unit tests

--- src/main/java/culturemedia/service/CultureMediaService.java
+++ src/main/java/culturemedia/service/CultureMediaService.java
@@ -2,12 +2,13 @@ package culturemedia.service;
 
 import java.util.List;
 
+import culturemedia.exception.VideoNotFoundException;
 import culturemedia.model.Video;
 import culturemedia.model.View;
 
 public interface CultureMediaService
 {
-    List<Video> findAll();
+    List<Video> findAll() throws VideoNotFoundException;
     Video add(Video video);
     View add(View view);
 }
