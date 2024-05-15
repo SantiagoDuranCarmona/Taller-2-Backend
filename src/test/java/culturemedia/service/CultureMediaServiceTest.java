080238b6b810eb854c3fd5d0de8027973c8e0f8f
SantiagoDC <sandurcar@gmail.com>
2024-05-15 00:22:53 -0500

    Added Mockito and refactored test methods in CultureMediaServiceTest

--- src/test/java/culturemedia/service/CultureMediaServiceTest.java
+++ src/test/java/culturemedia/service/CultureMediaServiceTest.java
@@ -2,53 +2,48 @@ package culturemedia.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import culturemedia.exception.VideoNotFoundException;
 import culturemedia.model.Video;
 import culturemedia.repository.VideoRepository;
 import culturemedia.repository.ViewsRepository;
-import culturemedia.repository.impl.VideoRepositoryImpl;
-import culturemedia.repository.impl.ViewsRepositoryImpl;
 import culturemedia.service.impl.CultureMediaServiceImpl;
 
-class CultureMediaServiceTest
+public class CultureMediaServiceTest
 {
-    private CultureMediaService cultureMediaService;
     private VideoRepository videoRepository;
     private ViewsRepository viewsRepository;
+    private CultureMediaService cultureMediaService;
+    private List<Video> videos;
 
     @BeforeEach
     void setUp() {
-        videoRepository = new VideoRepositoryImpl();
-        viewsRepository = new ViewsRepositoryImpl();
+        videoRepository = Mockito.mock(VideoRepository.class);
+        viewsRepository = Mockito.mock(ViewsRepository.class);
         cultureMediaService = new CultureMediaServiceImpl(videoRepository, viewsRepository);
-    }
-
-    private void createVideos() {
-        List<Video> videos = List.of(
+    
+        videos = List.of(
             new Video("01", "title 1", "----", 2.7),
-			new Video("02", "title 2", "----", 3.7),
-			new Video("03", "title 3", "----", 2.6),
-			new Video("04", "clip 1", "----", 1.7),
-			new Video("05", "clip 2", "----", 3.9),
-			new Video("06", "clip 3", "----", 3.3)
+            new Video("02", "title 2", "----", 3.7),
+            new Video("03", "title 3", "----", 2.6),
+            new Video("04", "clip 1", "----", 1.7),
+            new Video("05", "clip 2", "----", 3.9),
+            new Video("06", "clip 3", "----", 3.3)
         );
-
-        for ( Video video : videos ) {
-            cultureMediaService.add( video );
-        }
     }
 
     @Test
     void when_FindAll_all_videos_should_be_returned_successfully() throws VideoNotFoundException {
-        createVideos();
-        List<Video> videos = cultureMediaService.findAll();
-        assertEquals(6, videos.size());
+        when( videoRepository.findAll() ).thenReturn( videos );
+        List<Video> result = cultureMediaService.findAll();
+        assertEquals(videos, result);
     }
 
     @Test
@@ -58,9 +53,11 @@ class CultureMediaServiceTest
 
     @Test
     void when_FindByTitle_existing_title_should_return_matching_videos() throws VideoNotFoundException {
-        createVideos();
-        List<Video> videosFound = cultureMediaService.find("title 1");
-        assertEquals(1, videosFound.size());
+        String title = "title 1";
+        when( videoRepository.find( title ) ).thenReturn( List.of(videos.get(0)) );
+        List<Video> result = cultureMediaService.find( title );
+        assertEquals(1, result.size());
+        assertEquals(videos.get(0), result.get(0));
     }
 
     @Test
@@ -70,9 +67,15 @@ class CultureMediaServiceTest
 
     @Test
     void when_FindByDuration_existing_range_should_return_matching_videos() throws VideoNotFoundException {
-        createVideos();
-        List<Video> videosFound = cultureMediaService.find(2.5, 3.5);
-        assertEquals(3, videosFound.size());
+        double minDuration = 2.5;
+        double maxDuration = 3.5;
+        when( videoRepository.find(minDuration, maxDuration) ).thenReturn(
+            List.of(videos.get(0), videos.get(2), videos.get(5)));
+        List<Video> result = cultureMediaService.find(minDuration, maxDuration);
+        assertEquals(3, result.size());
+        assertEquals(videos.get(0), result.get(0));
+        assertEquals(videos.get(2), result.get(1));
+        assertEquals(videos.get(5), result.get(2));
     }
 
     @Test
